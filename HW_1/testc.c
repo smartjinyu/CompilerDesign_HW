6a517f0a5a4c936e587355168624a6f4e8fd352e
smartjinyu <smartjinyu@gmail.com>
2017-04-12 19:23:22 +0800

    prepare for hand in

--- HW_1/testc.c
+++ HW_1/testc.c
@@ -1,17 +1,18 @@
 //This test case is only for homework explanation
-#pragma source off
 int main () {
         double a = 6.0;
-        int i;
+        int *i={1,2,3};
+        char* d = "pragma source off";
         int b[2];
         for (i = 0; i < 2; i++) {
                 b[i] = i;
         }
-	char ss[]="0";
+		char ss='0';
         printf("b[1]=%d\n", b[1]);
         if (b[0] > 1){ 
                 a = a * 1.23e-1; 
-        }  
+        }
+        printf("fafafafafafafaafafafa");  
         return 0;
 
 } 
